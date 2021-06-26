f0bfbbc3e88b2f6c5edd09d96d65e568fc2f0bce
Your Name <ryanhemmila@gmail.com>
2021-06-26 12:17:01 -0500

    Able to run c code!

--- Makefile
+++ Makefile
@@ -1,14 +1,27 @@
 OUT_O_DIR := ./bin
+SRC_DIR := ./src
 
-run: $(OUT_O_DIR)/boot_sect.bin
-	qemu-system-x86_64 $(OUT_O_DIR)/boot_sect.bin
+run: $(OUT_O_DIR)/os_image.bin
+	qemu-system-i386 -fda $<
 
-build: $(OUT_O_DIR)/boot_sect.bin
+build: $(OUT_O_DIR)/os_image.bin
 
 clean:
 	rm -rf $(OUT_O_DIR)
 
+$(OUT_O_DIR)/kernel.bin: $(OUT_O_DIR)/kernel_entry.o $(OUT_O_DIR)/kernel.o
+	i386-elf-ld -o $@ -Ttext 0x1000 $^ --oformat binary
+
+$(OUT_O_DIR)/kernel_entry.o: $(SRC_DIR)/kernel_entry.asm
+	nasm $< -f elf -o $@
+
+$(OUT_O_DIR)/kernel.o: $(SRC_DIR)/kernel.c
+	i386-elf-gcc -ffreestanding -c $< -o $@
+
 $(OUT_O_DIR)/boot_sect.bin: $(wildcard ./src/*.asm)
 	mkdir -p $(OUT_O_DIR)
 	nasm -f bin ./src/boot_sect.asm -o $(OUT_O_DIR)/boot_sect.bin
+
+$(OUT_O_DIR)/os_image.bin: $(OUT_O_DIR)/boot_sect.bin $(OUT_O_DIR)/kernel.bin
+	cat $^ > $@
 
--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,14 +1,19 @@
 [org 0x7c00]
-mov bp, 0x9000 ; set stack base
-mov sp, bp     ; set start to base
+KERNEL_OFFSET equ 0x1000
 
-; say we're in real mode
+mov [BOOT_DRIVE], dl
+
+; setup stack
+mov bp, 0x9000
+mov sp, bp
+
+; log in real mode
 mov bx, REAL_MODE
 call print
+call print_nl
 
-; switch to 32-bit
+call load_kernel
 call switch
-jmp $
 
 ; includes
 %include "./src/gdt.asm"
@@ -18,14 +23,30 @@ jmp $
 %include "./src/print_hex.asm"
 %include "./src/32bit_print.asm"
 
+[bits 16]
+load_kernel:
+  mov bx, LOAD_KERNEL
+  call print
+  call print_nl
+
+  mov bx, KERNEL_OFFSET
+  mov dh, 2
+  mov dl, [BOOT_DRIVE]
+  call disk_load
+  ret
+
 [bits 32]
 BEGIN_PM:
   mov ebx, PROT_MODE
   call print_prot
+  call KERNEL_OFFSET
   jmp $
 
+BOOT_DRIVE db 0
 REAL_MODE db "Started in 16-bit real mode", 0
 PROT_MODE db "Switched to 32-bit protected mode", 0
+LOAD_KERNEL db "Loading kernel from disk", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
